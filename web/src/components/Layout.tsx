dd1402487d5b04a89aee9be9a5d7b3f8745ddd71
tekkenthuuug <sa.soto317@gmail.com>
2021-12-28 03:21:39 +0100

    impl: remove new site alert

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,16 +1,14 @@
 import { Box } from '@chakra-ui/core';
 import React from 'react';
 import NavBar from './NavBar';
-import NewSiteAlert from './NewSiteAlert';
 
-interface Props {}
+interface Props { }
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <Box maxWidth='800px' width='95%' mx='auto'>
       <Box as='header' my={3}>
         <NavBar />
-        <NewSiteAlert />
       </Box>
       {children}
     </Box>
